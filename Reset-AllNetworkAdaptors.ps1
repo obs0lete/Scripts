a2e07ebf53eab40fe7dbc0af5c868dfe9474ea4d
obs0lete <18431772+obs0lete@users.noreply.github.com>
2021-12-31 14:14:14 -0500

    Added check for eleveted prompt

--- Reset-AllNetworkAdaptors.ps1
+++ Reset-AllNetworkAdaptors.ps1
@@ -1,7 +1,16 @@
 # Quick script that will reset all the network adaptors.
 # you can run as-is or save it to your PowerShell profile.
 # Note that this will require an elevated prompt to run.
+
 process {
+    # Test if we are running as an administrator.
+    $ErrorActionPreference = "Stop"
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
+        [Security.Principal.WindowsBuiltInRole] "Administrator"))
+        {
+            Write-Warning "You are not running PowerShell with Administrator Rights.`nPlease re-launch PowerShell with Administrator Rights."
+            break
+        }
     $adaptors = Get-NetAdapter | Select-Object -ExpandProperty Name
     forEach ($i in $adaptors) {
         Write-Host "Disabling $i..."
